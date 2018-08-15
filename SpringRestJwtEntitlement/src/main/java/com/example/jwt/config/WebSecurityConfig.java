4eaa2fc215a83a0ee73d89bfbfd50a1918785b38
Krishna <1148983+krishnamanchikalapudi@users.noreply.github.com>
2018-08-15 09:32:13 -0700

    Create WebSecurityConfig.java

---
+++ SpringRestJwtEntitlement/src/main/java/com/example/jwt/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.jwt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // disable caching
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        // Create a default account
+    }
+
+}
