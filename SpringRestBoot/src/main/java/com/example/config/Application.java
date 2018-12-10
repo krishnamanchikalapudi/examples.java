e675ba9360aae56ef96650d1c1787e31c014eeb3
Krishna Manchikalapudi <krishna.manchikalapudi@gmail.com>
2018-12-09 17:10:23 -0800

    repo merge

---
+++ SpringRestBoot/src/main/java/com/example/config/Application.java
@@ -0,0 +1,22 @@
+package com.example.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.example")
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
